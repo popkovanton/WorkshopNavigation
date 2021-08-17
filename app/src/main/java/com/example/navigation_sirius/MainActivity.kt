30aaae4c2e88ec481188fcbbda1678e86891359a
a.s.popkov <a.s.popkov@tinkoff.ru>
2021-08-17 15:36:12 +0300

    Auth logic integrated

--- app/src/main/java/com/example/navigation_sirius/MainActivity.kt
+++ app/src/main/java/com/example/navigation_sirius/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.navigation_sirius
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class MainActivity : AppCompatActivity() {
+
+    private val buttonPush by lazy(LazyThreadSafetyMode.NONE) { findViewById<Button>(R.id.btnPush) }
+
+    private val loginResultHandler =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result?.data)
+            val account = task.result
+
+            startSecondActivity(account)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        buttonPush.setOnClickListener {
+            loginResultHandler.launch(getSignInIntent())
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        startSecondActivity(account)
+    }
+
+    private fun getSignInIntent(): Intent {
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        return mGoogleSignInClient.signInIntent
+    }
+
+    private fun startSecondActivity(account: GoogleSignInAccount?) {
+        if (account != null) {
+            buttonPush.visibility = View.INVISIBLE
+
+            // start second activity
+        }
     }
 }
